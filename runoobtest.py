a59af1a3595596df30f771cc303ca0c0797a51fd
taozi <1239963771@qq.com>
2022-03-17 00:07:44 +0800

    update

--- runoobtest.py
+++ runoobtest.py
@@ -49,14 +49,76 @@
 
 
 
-i = int(input('净利润:'))
-arr = [1000000, 600000, 400000, 200000, 100000, 0]
-rat = [0.01, 0.015, 0.03, 0.05, 0.075, 0.1]
-r = 0
-for idx in range(0, 6):
-    if i > arr[idx]:
-        r += (i - arr[idx]) * rat[idx]
-        print((i - arr[idx]) * rat[idx])
-        i = arr[idx]
-print(r)
+# i = int(input('净利润:'))
+# arr = [1000000, 600000, 400000, 200000, 100000, 0]
+# rat = [0.01, 0.015, 0.03, 0.05, 0.075, 0.1]
+# r = 0
+# for idx in range(0, 6):
+#     if i > arr[idx]:
+#         r += (i - arr[idx]) * rat[idx]
+#         print((i - arr[idx]) * rat[idx])
+#         i = arr[idx]
+# print(r)
 
+'''
+给定一个整数n，执行以下条件操作：
+如果n是奇数，打印Weird
+如果n是偶数并且在包含的范围内2到5，打印Not Weird
+如果n是偶数并且在包含的范围内6到20，打印Weird
+如果n甚至大于20， 打印Not Weird
+'''
+
+
+# n = int(input('请输入整数n：'))
+# if n%2==1 and n>=1 and n<=100:
+#     print('Weird')
+# elif n%2 == 0 and n>2 and n<5:
+#     print('Not Weird')
+# elif n%2 == 0 and n>6 and n<20:
+#     print('Weird')
+# elif n > 20 and n<=100:
+#     print('Not Weird')
+# else:
+#     print('输入错误！')
+
+
+'''
+已知一个字符串为 “hello_world_yoyo”, 如何得到一个队列 [“hello”,”world”,”yoyo”]
+'''
+
+# l = 'hello_world_yoyo'
+# list = l.split('_')
+# print(list)
+
+'''
+有个列表 [“hello”, “world”, “yoyo”]如何把把列表里面的字符串联起来，得到字符串 “hello_world_yoyo”
+'''
+
+list = ['hello', 'world', 'yoyo']
+# l = "_".join(list)
+# print(l)
+
+
+# j = ''
+# for l in list:
+#    j = j+'_'+l
+#
+# print(j)
+# print(j.lstrip('_'))
+
+'''
+把字符串 s 中的每个空格替换成”%20”
+输入：s = “We are happy.”
+输出：”We%20are%20happy.”
+'''
+# s = 'We are happy.'
+# print(s.replace(' ','%20'))
+
+'''
+打印99乘法表
+'''
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('{}*{}={}'.format(j,i,i*j),end=' ')
+    print('')
